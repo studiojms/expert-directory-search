2ccbaa819d5969a534d505efc8817b4946d5d219
Jefferson M Souza <jefferson.msouza86@gmail.com>
2021-04-17 22:41:59 -0300

    create page to include member

---
+++ frontend/src/components/pages/CreateMember/CreateMember.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import CreateMember from './CreateMember';
+
+describe('<CreateMember />', () => {
+  beforeEach(() => {
+    render(<CreateMember />);
+  });
+
+  it('should show two fields for user to fill with information', () => {
+    const inputs = screen.getAllByRole('textbox');
+
+    expect(inputs.length).toBe(2);
+  });
+
+  it('should show two buttons for user to control the form', () => {
+    const buttons = screen.getAllByRole('button');
+
+    expect(buttons.length).toBe(2);
+
+    const btnCancel = screen.getByText('Cancel');
+    expect(btnCancel).toBeInTheDocument();
+
+    const btnSave = screen.getByText('Save');
+    expect(btnSave).toBeInTheDocument();
+  });
+});
---
+++ frontend/src/components/pages/CreateMember/CreateMember.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router';
+
+function CreateMember() {
+  const [name, setName] = useState('');
+  const [website, setWebsite] = useState('');
+  const history = useHistory();
+
+  return (
+    <>
+      <h3>Create Member</h3>
+      <form className="app-form">
+        <fieldset>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Ex: John Doe"
+              value={name || ''}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="website">Website</label>
+            <input
+              id="website"
+              name="website"
+              type="text"
+              placeholder="Ex: http://www.google.com"
+              value={website || ''}
+              onChange={(e) => setWebsite(e.target.value)}
+            />
+          </div>
+          <div className="action-control">
+            <button
+              type="button"
+              className="app-btn-cancel"
+              onClick={() => {
+                history.push('/');
+              }}
+            >
+              Cancel
+            </button>
+
+            <button type="button" className="app-btn-primary">
+              Save
+            </button>
+          </div>
+        </fieldset>
+      </form>
+    </>
+  );
+}
+
+export default CreateMember;
