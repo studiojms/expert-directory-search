aec53a6afbf53448ebee61e976b58618ddd60b88
Jefferson M Souza <jefferson.msouza86@gmail.com>
2021-04-20 21:45:23 -0300

    [overtime] using jsoup to parse and extract html data

--- backend/src/main/java/com/studiojms/expertdirectoryapi/service/WebsiteProcessService.java
+++ backend/src/main/java/com/studiojms/expertdirectoryapi/service/WebsiteProcessService.java
@@ -1,12 +1,24 @@
 package com.studiojms.expertdirectoryapi.service;
 
+import com.studiojms.expertdirectoryapi.controller.MemberController;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class WebsiteProcessService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(WebsiteProcessService.class);
 
     /**
      * Given a url, fetches its content and gets all headings (h1 to h3)
@@ -15,8 +27,25 @@ public class WebsiteProcessService {
      * @return list with all headings (h1 to h3) found in the page
      */
     public List<String> scrapUrlHeadings(String url) {
-        //TODO use jsoup to scrap url headings
-        return Arrays.asList("TEST", "ASFD");
+        final List<String> headings = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(url).get();
+
+            Elements elements = new Elements();
+            elements.addAll(doc.getElementsByTag("h1"));
+            elements.addAll(doc.getElementsByTag("h2"));
+            elements.addAll(doc.getElementsByTag("h3"));
+
+            headings.addAll(elements.stream()
+                    .map(Element::text)
+                    .filter(e -> !e.isEmpty())
+                    .collect(Collectors.toList()));
+
+        } catch (IOException exception) {
+            LOGGER.error("There was an error parsing the url {}", url, exception);
+        }
+
+        return headings;
     }
 
     /**
